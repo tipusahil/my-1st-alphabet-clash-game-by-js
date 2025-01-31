e8d58a382df67b315bb6d8d72679eb2d02f98756
tipusahil <tipusahil.ctg@gmail.com>
2025-02-01 01:54:39 +0600

    keypress event add

--- app/utility_2.js
+++ app/utility_2.js
@@ -35,37 +35,45 @@ return random_1_alphabet;
 }
 
 
-// prottekbar jate new new alphabet p tag e set kore dei tar jonno loop: 
-function  continueGAME(){
-  const random_1_letter =  get_A_Random_AlphabetS();
-  console.log(random_1_letter);
-
-const set_random_1_letter =document.getElementById('random_letter');
-set_random_1_letter.innerHTML = random_1_letter;
-
-// random sonka jeta hobe oi mane jei key button id html e newa hoise,jmn a btn er jonno a name id newa hoise evabe oi keybtn er id er maddome oi btn ta ta js e select kora hobe.then setar backgroundColor golder kora hbe :
-const keyCOLOR = document.getElementById(random_1_letter);
-keyCOLOR.style.backgroundColor ='#e19f5d';
-keyCOLOR.style.color = 'black';
-
-
-
-// user clickedk key & random key soman hole loop abr colbe & socre 1 kroe barbe &.color green hobe.
-// const user_pressedKEY = keypressedLETTER();
-// console.log(user_pressedKEY);
-} 
-
-continueGAME();
-
 // callback function for user_clicked key 
 function getKeyPressLETTER(event){
 const playerPressedKEY = event.key;
 console.log('player pressed key is = ',playerPressedKEY);
-}
-document.addEventListener('keyup' , getKeyPressLETTER);//
-
 
 // get the expected to press :
 const  currentScreenAlphabet =document.getElementById('random_letter');
 const currentScreenLetter = currentScreenAlphabet.innerHTML;
 console.log('current screen letter is =',currentScreenLetter);
+
+let life = 10;
+let score = 0;
+if( currentScreenLetter === playerPressedKEY) {
+    console.log('yes yes')
+    life--;
+}else{
+    console.log('no no')
+    score++;
+}
+
+
+}
+document.addEventListener('keyup' , getKeyPressLETTER);//
+
+
+
+// prottekbar jate new new alphabet p tag e set kore dei tar jonno loop: 
+function  continueGAME(){
+    const random_1_letter =  get_A_Random_AlphabetS();
+    console.log(random_1_letter);
+  
+  const set_random_1_letter =document.getElementById('random_letter');
+  set_random_1_letter.innerHTML = random_1_letter;
+  
+  // random sonka jeta hobe oi mane jei key button id html e newa hoise,jmn a btn er jonno a name id newa hoise evabe oi keybtn er id er maddome oi btn ta ta js e select kora hobe.then setar backgroundColor golder kora hbe :
+  const keyCOLOR = document.getElementById(random_1_letter);
+  keyCOLOR.style.backgroundColor ='#e19f5d';
+  keyCOLOR.style.color = 'black';
+
+  } 
+  
+  continueGAME();
